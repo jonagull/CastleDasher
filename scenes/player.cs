9216923285046449d1fd76f139c9b87095301d08
Boien <59290906+jonagull@users.noreply.github.com>
2026-03-11 16:38:05 +0100

    basic movement works

--- scenes/player.cs
+++ scenes/player.cs
@@ -1,7 +1,6 @@
 using Godot;
-using System;
 
-public partial class Player : Node
+public partial class Player : CharacterBody3D
 {
 	// How fast the player moves in meters per second.
 	[Export]
@@ -11,13 +10,17 @@ public partial class Player : Node
 	public int FallAcceleration { get; set; } = 75;
 
 	private Vector3 _targetVelocity = Vector3.Zero;
-	
+
+	public override void _Ready()
+	{
+		GD.Print("Player::Godot");
+	}
+
 	public override void _PhysicsProcess(double delta)
 	{
-		// We create a local variable to store the input direction.
+
 		var direction = Vector3.Zero;
 
-		// We check for each move input and update the direction accordingly.
 		if (Input.IsActionPressed("move_right"))
 		{
 			direction.X += 1.0f;
@@ -28,13 +31,32 @@ public partial class Player : Node
 		}
 		if (Input.IsActionPressed("move_back"))
 		{
-			// Notice how we are working with the vector's X and Z axes.
-			// In 3D, the XZ plane is the ground plane.
 			direction.Z += 1.0f;
 		}
 		if (Input.IsActionPressed("move_forward"))
 		{
 			direction.Z -= 1.0f;
 		}
+
+		if (direction != Vector3.Zero)
+		{
+			direction = direction.Normalized();
+			// Setting the basis property will affect the rotation of the node.
+			GetNode<Node3D>("Pivot").Basis = Basis.LookingAt(direction);
+		}
+
+		// Ground velocity
+		_targetVelocity.X = direction.X * Speed;
+		_targetVelocity.Z = direction.Z * Speed;
+
+		// Vertical velocity
+		if (!IsOnFloor()) // If in the air, fall towards the floor. Literally gravity
+		{
+			_targetVelocity.Y -= FallAcceleration * (float)delta;
+		}
+
+		// Moving the character
+		Velocity = _targetVelocity;
+		MoveAndSlide();
 	}
 }
